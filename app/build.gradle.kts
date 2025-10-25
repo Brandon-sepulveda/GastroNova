b024724b8feff1c6895db32caa2bb595fed41a49
Brandon-sepulveda <bran.sepulvedac@duocuc.cl>
2025-10-25 19:11:52 -0300

    Se agrego view "Registrar restaurant"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
--- app/src/main/java/com/example/gastronova/MainActivity.kt
+++ app/src/main/java/com/example/gastronova/MainActivity.kt
@@ -1,28 +1,37 @@
 package com.example.gastronova
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -42,11 +51,14 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.example.gastronova.ui.theme.GastroNovaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +67,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GastroNovaTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    AppNavigation()
+                    AppNavigation(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -66,13 +78,15 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun AppNavigation() {
+fun AppNavigation(modifier: Modifier) {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = "login") {
         composable("login") {Login(navController)}
         composable("register") {Register(navController)}
         composable ("Opcion"){Opcion(navController)  }
+        composable ("RegistrarRestaurant"){RegistrarRestaurant(navController)  }
+
 
     }}
 
@@ -155,7 +169,7 @@ fun Login(navController: NavHostController) {
 fun Register(navController: NavHostController) {
     var usuario by remember { mutableStateOf("") }
     var contrasena by remember { mutableStateOf("") }
-    var rep_contrasena by remember { mutableStateOf("") }
+    var repContrasena by remember { mutableStateOf("") }
     var nombre by remember { mutableStateOf("") }
     var apellido by remember { mutableStateOf("") }
     var correo by remember { mutableStateOf("") }
@@ -230,8 +244,8 @@ fun Register(navController: NavHostController) {
                 )
                 //REPETIR CONTRASEÑA
                 OutlinedTextField(
-                    value = rep_contrasena,
-                    onValueChange = { rep_contrasena = it },
+                    value = repContrasena,
+                    onValueChange = { repContrasena = it },
                     label = { Text("Repetir contraseña") },
                     visualTransformation = PasswordVisualTransformation(),
                     modifier = Modifier.fillMaxWidth(),
@@ -298,7 +312,7 @@ fun Opcion(navController: NavHostController) {
 
 
                 Button(
-                    onClick = { /* Acción al hacer clic en el botón */ },
+                    onClick = { navController.navigate("RegistrarRestaurant") },
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(16.dp)
                 ) {
@@ -333,30 +347,213 @@ fun Opcion(navController: NavHostController) {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegistrarRestaurant(navController: NavHostController) {
+    var nombreRestaurant by remember { mutableStateOf("") }
+    var empresa by remember { mutableStateOf("") }
+    var ubicacion by remember { mutableStateOf("") }
+    var tipoRestaurant by remember { mutableStateOf("") }
+    var descripcion by remember { mutableStateOf("") }
+    var imagen by remember { mutableStateOf<Uri?>(null) }
 
+    // Para el menu de seleccion
+    val tipos=listOf("Italiano","Mexicano","Japones","Asiatico","China","Peruana","Molecular","Argentia","Vegana")
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.gato_mapa),
+            contentDescription = "GatoMapa",
+            modifier = Modifier
+                .align(Alignment.Center).padding(start = 110.dp).size(125.dp),
+            contentScale = ContentScale.Fit
+
+        )
+    }
+    Box(modifier = Modifier.fillMaxSize().padding(top = 125.dp),){
+        Card(modifier = Modifier.align(Alignment.TopCenter)
+            .padding(16.dp)
+            .fillMaxWidth(0.9f),
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)) {
+            Column(modifier = Modifier.padding(16.dp).verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                Text(
+                    text = "Registrar restaurant",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align (Alignment.CenterHorizontally)
+                )
+                //NOMBRE DEL RESTAURANTE
+                OutlinedTextField(
+                    value = nombreRestaurant,
+                    onValueChange = { nombreRestaurant = it },
+                    label = { Text("Nombre del restaurante") },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    singleLine = true
+                )
+                //NOMBRE DE LA EMPRESA
+                OutlinedTextField(
+                    value = empresa,
+                    onValueChange = { empresa = it },
+                    label = { Text("Nombre de la empresa") },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    singleLine = true
+                )
+                //DIRECCION DEL RESTAURANT
+                OutlinedTextField(
+                    value = ubicacion,
+                    onValueChange = { ubicacion = it },
+                    label = { Text("Dirección del restaurante") },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    singleLine = true
+                )
+                //TIPO DE RESTAURANTE
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .zIndex(1f)
+                ) {
+                    OutlinedTextField(
+                        value = tipoRestaurant,
+                        onValueChange = { },              // readOnly
+                        readOnly = true,
+                        label = { Text("Tipo de restaurant") },
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded)
+                        },
+                        shape = RoundedCornerShape(16.dp),
+                        singleLine = true,
+                        modifier = Modifier
+                            .menuAnchor()
+                            .fillMaxWidth()
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        tipos.forEach { opcion ->
+                            DropdownMenuItem(
+                                text = { Text(opcion) },
+                                onClick = {
+                                    tipoRestaurant = opcion
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+                }
+
+                //DESCRIPCION
+                OutlinedTextField(
+                    value = descripcion,
+                    onValueChange = { descripcion = it },
+                    label = { Text("Descripcion del restaurant") },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp)
+                )
+
+                //IMAGEN
+                CampoAdjuntarImagen(
+                    imagenUri = imagen,
+                    onImagenSeleccionada = { imagen = it }
+                )
+
+                Button(
+                    onClick = { navController.navigate("Opcion")},
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Text(text = "Registrar restaurante")
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun CampoAdjuntarImagen(
+    imagenUri: Uri?,
+    onImagenSeleccionada: (Uri?) -> Unit
+) {
+    // Lanzador del Photo Picker (Android 13+; en 33+ no requiere permisos)
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        onImagenSeleccionada(uri) // guarda el Uri seleccionado (o null si canceló)
+    }
+
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        OutlinedButton(
+            onClick = {
+                launcher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Adjuntar imagen")
+        }
+
+        // Vista previa (opcional): miniatura cuando hay imagen seleccionada
+        if (imagenUri != null) {
+            Card(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                AsyncImage(
+                    model = imagenUri,
+                    contentDescription = "Vista previa",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(180.dp)
+                )
+            }
+        }
+    }
+}
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun RegisterScreenPreview() {
-    val navController = rememberNavController() // 👈 evita null en preview
+    val navController = rememberNavController()
     GastroNovaTheme {
         Register(navController)
     }
 }
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-private fun loginScreenPreview() {
-    val navController = rememberNavController() // 👈 evita null en preview
+private fun LoginScreenPreview() {
+    val navController = rememberNavController()
     GastroNovaTheme {
         Login(navController)
     }
 }
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-private fun opcionScreenPreview() {
-    val navController = rememberNavController() // 👈 evita null en preview
+private fun OpcionScreenPreview() {
+    val navController = rememberNavController()
     GastroNovaTheme {
         Opcion(navController)
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun RegistrarRestaurantScreenPreview() {
+    val navController = rememberNavController()
+    GastroNovaTheme {
+        RegistrarRestaurant(navController)
     }
 }
